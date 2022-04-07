a3aa7cb4465cb28babdc54f41fd188a5d541a132
Bongani <mkhumbie34@gmail.com>
2022-04-07 23:55:21 +0200

    main pic add

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -1,7 +1,12 @@
 const burger = document.querySelectorAll(`.burger`)
 const social = document.querySelector(`.left`)
 const menu = document.getElementById(`menu`)
+const lineH= document.querySelector(`.line-h`)
+const burgerLineOne = document.querySelector(`.burger`)
+const burgerLineThree = document.querySelector(`.burger:nth-child(3)`)
+const test = document.querySelector(`.main-pic`)
 
+test.style.backgroundImage=`url("../images/main-long.jpg")`
 
 
 for(const line of burger){
@@ -9,12 +14,24 @@ for(const line of burger){
     line.addEventListener(`click`,()=>{
         if(enabler == 0){
             social.classList.add(`display-none`)
-            menu.classList.add(`menu-open`)  
+            menu.classList.add(`menu-open`)
+            lineH.classList.add(`display-none`)
+            
+            
+            burgerLineOne.classList.add(`line-1`)
+            burgerLineThree.classList.add(`line-3`)
+
+            
+            //allow to toggle between states 
             enabler = enabler +1
         }
         else{
             social.classList.remove(`display-none`)
             menu.classList.remove(`menu-open`)
+            lineH.classList.remove(`display-none`)
+
+            burgerLineOne.classList.remove(`line-1`)
+            burgerLineThree.classList.remove(`line-3`)
             enabler = 0
         }
     }
