b2bd22233fc79adf83f8c20a1a47a2b868c14ebc
Bongani <mkhumbie34@gmail.com>
2022-04-07 15:53:59 +0200

    menu toggle cont

--- assets/scripts/menu.js
+++ assets/scripts/menu.js
@@ -5,11 +5,18 @@ const menu = document.getElementById(`menu`)
 
 
 for(const line of burger){
+    let enabler = 0
     line.addEventListener(`click`,()=>{
-        social.style.display="none"
-
-        menu.classList.add(`menu-open`)
-
+        if(enabler == 0){
+            social.classList.add(`display-none`)
+            menu.classList.add(`menu-open`)  
+            enabler = enabler +1
+        }
+        else{
+            social.classList.remove(`display-none`)
+            menu.classList.remove(`menu-open`)
+            enabler = 0
+        }
     }
     )
 }
